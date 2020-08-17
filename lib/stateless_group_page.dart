f56096bd9a66bce1c6a5e651440c456411f1553a
hyc <18956532506@163.com>
2020-08-17 16:12:20 +0800

    feat: stateless组件

---
+++ lib/stateless_group_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LessGroupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = TextStyle(fontSize: 20);
+    return MaterialApp(
+      title: 'StatelessWeight与基础组件',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("StatelessWeight与基础组件"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(color: Colors.white),
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              Text(
+                "i am Text",
+                style: textStyle,
+              ),
+              Icon(
+                Icons.android,
+                size: 50,
+                color: Colors.cyan,
+              ),
+              CloseButton(),
+              BackButton(),
+              Chip(
+                avatar: Icon(Icons.people),
+                label: Text('StatelessWeight与基础组件'),
+              ),
+              Divider(
+                height: 10,
+                indent: 10,
+                color: Colors.amberAccent,
+              ),
+              Card(
+                color: Colors.white,
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'i am card',
+                    style: textStyle,
+                  ),
+                ),
+              ),
+              AlertDialog(
+                title: Text('title'),
+                content: Text('加夫里什健康'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
