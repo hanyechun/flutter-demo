62ffc98f6be90a3c0f736e009e7c4307c75a7a11
hyc <18956532506@163.com>
2020-08-19 18:45:30 +0800

    refactor: 新增banner插件

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +7,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrl = [
+    'http://www.devio.org/img/avatar.png',
+    'http://www.devio.org/img/avatar.png',
+    'http://www.devio.org/img/avatar.png'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('首页'),
+      child: Scaffold(
+          body: Column(
+        children: [
+          Container(
+            height: 160,
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return Image.network(
+                  _imageUrl[index],
+                  fit: BoxFit.fill,
+                );
+              },
+              pagination: SwiperPagination(),
+              itemCount: _imageUrl.length,
+              autoplay: true,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
