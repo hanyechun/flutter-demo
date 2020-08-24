b79bd79409b4707f432025281c71eae5a6a7bca9
hyc <18956532506@163.com>
2020-08-24 10:20:09 +0800

    feat: GridView widget的使用

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -5,11 +5,33 @@ class TravelPage extends StatefulWidget {
   _TravelPageState createState() => _TravelPageState();
 }
 
+const CITY_NAME = ['粉丝', '刚发的', '刚发的', '换个', '火凤凰', '好烦好烦', '火凤凰更好'];
+
 class _TravelPageState extends State<TravelPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('旅拍'),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: _buildCities(),
+      ),
+    );
+  }
+
+  List<Widget> _buildCities() {
+    return CITY_NAME.map((city) => _item(city)).toList();
+  }
+
+  Widget _item(String city) {
+    return Container(
+      height: 50,
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(bottom: 5),
+      decoration: BoxDecoration(color: Colors.teal),
+      child: Text(
+        city,
+        style: TextStyle(color: Colors.white),
+      ),
     );
   }
 }
