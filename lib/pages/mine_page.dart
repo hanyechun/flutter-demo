be22f59af06433bd2ec372b3677ac14c4852a693
hyc <18956532506@163.com>
2020-08-24 11:36:02 +0800

    feat: ListView下拉刷新、上拉加载

--- lib/pages/mine_page.dart
+++ lib/pages/mine_page.dart
@@ -6,10 +6,68 @@ class MinePage extends StatefulWidget {
 }
 
 class _MinePageState extends State<MinePage> {
+  List<String> cityNames = ['不过风景', '噶色呃', '并；沙发上', '买内裤女', '测是否', '测', '管理的'];
+  ScrollController _controller = ScrollController();
+
+  @override
+  void initState() {
+    _controller.addListener(() {
+      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+        _loadData();
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('我的'),
+      child: RefreshIndicator(
+        child: ListView(
+          controller: _controller,
+          children: _buildCities(),
+        ),
+        onRefresh: _handleRefresh,
+      ),
     );
+  }
+
+  Future<Null> _handleRefresh() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    setState(() {
+      cityNames = cityNames.reversed.toList();
+    });
+    return null;
+  }
+
+  _loadData() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    setState(() {
+      List<String> list = List<String>.from(cityNames);
+      list.addAll(cityNames);
+      cityNames = list;
+    });
+  }
+
+  List<Widget> _buildCities() {
+    return cityNames.map((city) => _item(city)).toList();
+  }
+
+  Widget _item(String city) {
+    return Container(
+      height: 150,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(color: Colors.green),
+      child: Text(
+        city,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
