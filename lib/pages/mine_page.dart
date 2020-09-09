96c97f16fc62a968a57b009d3d7bed67b56b4862
hyc <18956532506@163.com>
2020-09-09 14:42:38 +0800

    refactor: 完善个人中心

--- lib/pages/mine_page.dart
+++ lib/pages/mine_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo/widgets/webview.dart';
 import 'package:flutter/material.dart';
 
 class MinePage extends StatefulWidget {
@@ -6,69 +7,15 @@ class MinePage extends StatefulWidget {
 }
 
 class _MinePageState extends State<MinePage> {
-  List<String> cityNames = ['不过风景', '噶色呃', '并；沙发上', '买内裤女', '测是否', '测', '管理的'];
-  ScrollController _controller = ScrollController();
-
-  @override
-  void initState() {
-    _controller.addListener(() {
-      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
-        _loadData();
-      }
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RefreshIndicator(
-        child: ListView(
-          controller: _controller,
-          children: _buildCities(),
-        ),
-        onRefresh: _handleRefresh,
+    return Scaffold(
+      body: WebView(
+        url: 'https://m.ctrip.com/webapp/myctrip',
+        statusBarColor: 0xff4c5bca,
+        hideAppBar: true,
+        backForbid: true,
       ),
     );
-  }
-
-  Future<Null> _handleRefresh() async {
-    await Future.delayed(Duration(milliseconds: 200));
-    setState(() {
-      cityNames = cityNames.reversed.toList();
-    });
-    return null;
-  }
-
-  _loadData() async {
-    await Future.delayed(Duration(milliseconds: 200));
-    setState(() {
-      List<String> list = List<String>.from(cityNames);
-      list.addAll(cityNames);
-      cityNames = list;
-    });
-  }
-
-  List<Widget> _buildCities() {
-    return cityNames.map((city) => _item(city)).toList();
-  }
-
-  Widget _item(String city) {
-    return Container(
-      height: 150,
-      alignment: Alignment.center,
-      margin: EdgeInsets.only(bottom: 5),
-      decoration: BoxDecoration(color: Colors.green),
-      child: Text(
-        city,
-        style: TextStyle(color: Colors.white),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 }
